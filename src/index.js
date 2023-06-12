6f878d10b54ec4574bbba08450e94bef4a1b85d5
futuristicbolt <136035511+futuristicbolt@users.noreply.github.com>
2023-06-12 17:25:08 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 // import { HashRouter } from 'react-router-dom';
 
+console.log(process.env.PUBLIC_URL)
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
